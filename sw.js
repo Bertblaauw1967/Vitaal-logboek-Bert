fea53ed4aaa207da193fac1bf8921b783834b44f
Bertblaauw1967 <bert.blaauw@hotmail.com>
2026-08-29 18:51:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vitaal-logboek-v2-dark';
+const CACHE='vitaal-logboek-v3-contrast';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
