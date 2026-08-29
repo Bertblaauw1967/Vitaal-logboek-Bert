9f27ec593b709a50aa001864005539eee6ce326e
Bertblaauw1967 <bert.blaauw@hotmail.com>
2026-08-29 20:42:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,10 @@
-const CACHE='leefkracht-v17';
-const STATIC=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-
-self.addEventListener('install',event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)));
-});
-
+self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>{
   event.waitUntil(
     caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-
-  const req=event.request;
-  const isNav=req.mode==='navigate' || (req.headers.get('accept')||'').includes('text/html');
-
-  if(isNav){
-    event.respondWith(
-      fetch(req,{cache:'no-store'})
-        .then(res=>res)
-        .catch(()=>caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    fetch(req)
-      .then(res=>{
-        const copy=res.clone();
-        caches.open(CACHE).then(cache=>cache.put(req,copy));
-        return res;
-      })
-      .catch(()=>caches.match(req))
+      .then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+      .then(()=>self.registration.unregister())
+      .then(()=>self.clients.matchAll())
+      .then(clients=>clients.forEach(c=>c.navigate(c.url)))
   );
 });
