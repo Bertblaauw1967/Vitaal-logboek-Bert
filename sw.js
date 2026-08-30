4e7317c4153dc7c1c68c901a41fc687016f4eb49
Bertblaauw1967 <bert.blaauw@hotmail.com>
2026-08-30 10:57:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,10 @@
-// LeefKracht 30.3 cachevrij
+// LeefKracht 30.4 - cachevrij
 self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil((async()=>{try{const k=await caches.keys();await Promise.all(k.map(x=>caches.delete(x)));await self.registration.unregister()}catch(x){}})()));
+self.addEventListener('activate',event=>event.waitUntil((async()=>{
+  try{
+    const keys=await caches.keys();
+    await Promise.all(keys.map(k=>caches.delete(k)));
+    await self.registration.unregister();
+  }catch(e){}
+})()));
 self.addEventListener('fetch',()=>{});
