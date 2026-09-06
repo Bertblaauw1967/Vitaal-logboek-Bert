a7eede4061310b17b4336b3441785bba06800b11
Bertblaauw1967 <bert.blaauw@hotmail.com>
2026-09-06 19:37:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/* Mijn LeefRitme 0.9 — stabiele, cache-arme service worker. */
+/* Mijn LeefRitme 0.9.1 — stabiele, cache-arme service worker. */
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
   event.waitUntil((async()=>{
